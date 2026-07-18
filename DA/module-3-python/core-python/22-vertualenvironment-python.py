7c50d4d1e6729de01db97e37c8f299960526881f
Brijesh1990 <bkpandey.pandey@gmail.com>
2026-07-18 08:55:18 +0530

    18-07-2026 all data uploaded

---
+++ DA/module-3-python/core-python/22-vertualenvironment-python.py
@@ -0,0 +1,41 @@
+"""
+A virtual environment in python is used to create a individual project separate environment
+
+A virtual env in python create a separate project
+
+A virtual env is created for separate project that can not be overlap with another
+
+
+How to create a virtual environment ....
+
+1. install virtual environment   
+   
+   cmd .. pip install virtualenv 
+          pip show virtualenv
+          
+2.create a virtualenv
+
+  cmd: virtualenv myapp    
+  
+3. switch in myapp virtualenv
+
+   cmd : cd myapp
+   
+4. activate virtualenv 
+
+   cmd: myapp>Scripts\activate
+   
+5. activated virtualenv 
+   
+   (myapp) E:\Data_Science&Data_Analytics_8am\DA\module-3-python\core-python\myapp>
+   
+6. create a python file in virtual env 
+
+   (myapp) E:\Data_Science&Data_Analytics_8am\DA\module-3-python\core-python\myapp>type nul > list_of_keyword.py
+   
+7. How to deactivate virtualenv
+
+   (myapp) E:\Data_Science&Data_Analytics_8am\DA\module-3-python\core-python\myapp>deactivate      
+                     
+
+"""
