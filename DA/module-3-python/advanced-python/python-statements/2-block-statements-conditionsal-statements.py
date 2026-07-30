25ddabd51d88bdf712bc5c861842cdae544de4a4
Brijesh1990 <bkpandey.pandey@gmail.com>
2026-07-30 08:16:00 +0530

    30-07-2026 all data uploaded

--- DA/module-3-python/advanced-python/python-statements/2-block-statements-conditionsal-statements.py
+++ DA/module-3-python/advanced-python/python-statements/2-block-statements-conditionsal-statements.py
@@ -113,12 +113,153 @@
 
 
 # take input from users
-a=int(input("Enter a values :"))
-b=int(input("Enter b values :"))
-if a>b:
-    print("a is greater than b")
-elif b>a:
-    print("b is greater than a")
-else:
-    print("a and b both are same") 
+# a=int(input("Enter a values :"))
+# b=int(input("Enter b values :"))
+# if a>b:
+#     print("a is greater than b")
+# elif b>a:
+#     print("b is greater than a")
+# else:
+#     print("a and b both are same") 
+
+
+
+# looping statements : 
+# looping statements are executed number of iteration repeat again and again.
+# loop is used to executed numbers of iteration again and again 
+
+# syntax
+# for i in range():
+#     statments
+
+# for i in range(1,10):
+#     print(i)
+
+# for i in range(1,100):
+#     print(i)
+
+# for i in range(1,100):
+#     print(i, end=" ")
+
+
+# for i in range(1,100):
+#     print(i, end =" , ")
+
+# loop with condition
+# w.a.p to print 1 to 10 and give only odd numbers 
+# for i in range(1,10):
+#     if i%2==0:
+#         print(i)
+# for i in range(1,10):
+#     if i%2==1:
+#         print(i)
+
+# for i in range(1,10):
+#     if i%2!=0:
+#         print(i)
+
+# employee={
+#     "id":1,
+#     "name":"brijesh",
+#     "age":35,
+#     "department":"IT",
+#     "address":"150 feet ring road rajkot"
+# }
+
+# for i in employee:
+#     print(employee[i])
+#     print(employee[i])
+
+
+# employee={
+#     "fname":["nimavat","aryan","om","amish"],
+#     "age":[25,21,21,30]
+# }
+# for i in employee:
+    # print(employee["fname"])
+    # print(employee["age"])
+    # print(employee[i])
+    # print(employee[i])
+    # print(employee[i][1])
+    # print(employee[i][0:1])
+    # print(employee[i][0:3])
+    # print(employee[i][0:4])
+    # print(employee["fname"][0:1])
+    
+# employee={
+#     "fname":["nimavat","aryan","om","amish"],
+#     "age":[25,21,21,30]
+# }
+
+# iterate only name 
+# for i in employee["fname"]:
+#     print(i)
+
+# for i in employee["age"]:
+#     print(i)
+    
+    
+# employee=["om","amish","aryan","kumar","lokesh","hitesh","astha"]
+# for i in employee:
+#     print(i)   
+
+
+# while : while is a loop that can be executed when condition is true 
+# syntax 
+# while condition:
+#     statements
+#     increments/decrements
+
+# i=0
+# while i<=10:
+#     print(i)
+#     i=i+1
+
+
+# i=0
+# while i<=10:
+#     print(i)
+#     i=i+1
+
+
+# i=0
+# while i<=10:
+#     if i==5:
+#         break
+#     print(i)
+#     i=i+1
+ 
+ 
+# i=0
+# while i<=10:
+#     if i==5 or i==8:
+#         i=i+1
+#         continue
+#     print(i)
+#     i=i+1    
+ 
+ 
+# i=0
+# while i<=10:
+#     if i  not in(5,8):
+#         print(i)
+#     i=i+1    
+
+# i=0
+# while i<=500:
+#     if i  not in(5,8):
+#         print(i)
+#     i=i+1    
+
+
+i=0
+while i<=10:
+    print(i)
+    i=i+2
+    
+  
+ 
+    
+     
+ 
 
---
+++ DA/module-3-python/advanced-python/python-statements/3-flow-control-statements.py
@@ -0,0 +1,16 @@
+# control the loop iteration 
+#  break | continue
+
+# for i in range(1,101):
+#     print(i)
+
+# for i in range(1,101):
+#     if i==51:
+#         break
+#     print(i)
+
+# numbers of iteration skip
+for i in range(1,101):
+    if i==10 or i==20 or i==30:
+        continue
+    print(i)
