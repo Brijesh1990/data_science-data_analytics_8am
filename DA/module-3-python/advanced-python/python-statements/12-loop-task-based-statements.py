18bc0a10d9708d4ac84f47000e63eb3cf66a1ed5
Brijesh1990 <bkpandey.pandey@gmail.com>
2026-08-22 17:13:42 +0530

    22-08-2026 all data uploaded

---
+++ DA/module-3-python/advanced-python/python-statements/12-loop-task-based-statements.py
@@ -0,0 +1,117 @@
+# w.a.p to check authentication for login with email and password with 3 consecutive wrong password your account will be blocked ...
+
+# em='admin@gmail.com'
+# pwd='admin@123'
+# for chk in range(1,4):
+#     em=input("Enter your email :")
+#     pwd=input("Enter your Password :")
+    
+#     if em=='admin@gmail.com' and pwd=='admin@123':
+#         print("you are Logged in successfully")
+#     else:
+#         print("your Login credentials are wrong")
+# else: 
+#     print("your account is blocked")
+
+
+# w.a.p tp print a star triangle pattern
+# *
+# **
+# ***
+# ****
+# *****
+
+# for i in range(1,6):
+#     for j in range(i+1):
+#         print("*",end="") #print one row
+#     print() #new line print
+        
+
+#w.a.p to print reversed star triangle
+
+# *****
+# ****
+# ***
+# **
+# *
+
+# n=5
+# for i in range(n,0,-1):
+#     for j in range(i):
+#         print("*",end="") #print one row
+#     #new line print
+#     print()
+         
+
+# w.a.p to print Rectangle
+# ******
+# ******
+# ******
+# ******
+# ******
+# ******
+
+# row=6
+# cols=8
+# for i in range(row):
+#     for j in range(cols):
+#         print("*", end="")
+#     print()
+
+
+# w.a.p to ATM transaction.......
+
+# balance=50000
+# while True:
+#     print("\n ----- Welcome in ATM -------")
+#     print("\n 1.----- check balance -------")
+#     print("\n 2.----- withdrawal -------")
+#     print("\n 3----- deposit -------")
+#     print("\n 4----- exit -------")
+#     choice=int(input("Enter your choice :"))
+
+#     if choice==1:
+#         print("Your account balance is :",balance)
+#     elif choice==2:
+#         amount=int(input("Enter your withdrawal amount :"))
+#         if balance>amount:
+#             res=balance-amount
+#             print("You remaining amount is :",res)
+#         else:
+#             print("insufficient amount for withdrawal check your balance")
+    
+#     elif choice==3:
+#           amount=int(input("Enter your Deposit amount :"))
+#           res=balance+amount
+#           print("You New Balance  is :",res)
+    
+#     else:
+#         print("you entered wrong choice")
+#         break
+
+
+# w.a.p to print 
+# 5
+# 4
+# 3
+# 2
+# 1
+
+# i=5
+# while i>0:
+#     print(i)
+#     i-=1
+
+# i=0
+# while i<=5:
+#     print(i)
+#     i+=1 #i=i+1
+  
+        
+
+          
+          
+        
+        
+    
+
